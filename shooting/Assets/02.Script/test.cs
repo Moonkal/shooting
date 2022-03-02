003fea0766eccbb06864ac3abc627983f491115e
Moonkal <99639280+Moonkal@users.noreply.github.com>
2022-03-02 21:56:21 +0900

    shooting2
    두번째

--- shooting/Assets/02.Script/test.cs
+++ shooting/Assets/02.Script/test.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    private Transform tr;
     // Start is called before the first frame update
+    public float movespeed = 10.0f;
+    public float turnspeed = 80.0f;
     void Start()
     {
-        
+        tr=GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -15,7 +18,12 @@ public class test : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
         Debug.Log("H:"+h);
         Debug.Log("v:"+v);
-    }
+
+        Vector3 movedir = (Vector3.forward * v) + ( Vector3.right * h);
+        tr.Translate(movedir * movespeed * Time.deltaTime);
+        tr.Rotate(Vector3.up * turnspeed * Time.deltaTime * r);
+    } 
 }
